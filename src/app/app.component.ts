36363d2a0a19bb92e59953e2c3204422e0daebf8
Diana Holik <diana_holik@epam.com>
2021-01-22 18:10:32 +0200

    implemented case with duplicated data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,9 @@ export class AppComponent {
   {"ID": 5, "Car type": 'SEDAN', "Brand": 'Toyota', "Model": 'Avalon', "Owner": 'Sidorov Oleg'},
   {"ID": 6, "Car type": 'MINIVAN', "Brand": 'Honda', "Model": 'Odyssey', "Owner": 'Ivanov Oleg'},
   {"ID": 7, "Car type": 'SEDAN', "Brand": 'Honda', "Model": 'Accord', "Owner": 'Petrov Oleg'},
-  {"ID": 8, "Car type": 'MINIVAN', "Brand": 'Chrysler', "Model": 'Pacifica', "Owner": 'Ivanov Oleg'},
+  {"ID": 8, "Car type": 'MINIVAN', "Brand": 'Honda', "Model": 'Odyssey', "Owner": 'Ivanov Oleg'},
   {"ID": 9, "Car type": 'SEDAN', "Brand": 'Kia', "Model": 'Rio', "Owner": 'Petrov Petr'},
-  {"ID": 10, "Car type": 'PICKUP TRUCK', "Brand": 'Honda', "Model": 'Ridgeline', "Owner": 'Ivanov Vasyl'}
+  {"ID": 10, "Car type": 'PICKUP TRUCK', "Brand": 'Honda', "Model": 'Ridgeline', "Owner": 'Ivanov Vasyl'},
+  {"ID": 11, "Car type": 'CONVERTIBLE', "Brand": 'Mazda', "Model": 'Miata', "Owner": 'Ivanov Ivan'}
   ]
 }
